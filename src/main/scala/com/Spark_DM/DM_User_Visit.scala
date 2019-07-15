5f1abb5d813f3025fc7b61631b8dd014da81d1e1
zddcv <511566945@qq.com>
2019-07-15 21:51:12 +0800

    写入完毕

--- src/main/scala/com/Spark_DM/DM_User_Visit.scala
+++ src/main/scala/com/Spark_DM/DM_User_Visit.scala
@@ -23,16 +23,16 @@ object DM_User_Visit {
         .debug("提交的表名参数有问题！请重新设置。。。")
     }else{
         // 处理SQL内部的占位符
-//      val finalSql = sql.replace("?",args(1))
+      val finalSql = sql.replace("?",args(1))
         // 运行SQL
-      val df = hiveContext.sql(sql)
+      val df = hiveContext.sql(finalSql)
       // 处理配置参数
       val mysqlTableName = args(0).split("\\.")(1)
       val hiveTableName = args(0)
       val jdbcProp = JDBCUtils.getJdbcProp()._1
       val jdbcUrl = JDBCUtils.getJdbcProp()._2
       // 存入MySQL
-      df.write.mode("append").jdbc(jdbcUrl,mysqlTableName,jdbcProp)
+      df.coalesce(1).write.mode("append").jdbc(jdbcUrl,mysqlTableName,jdbcProp)
       // 存入Hive
       //df.coalesce(1).write.mode(SaveMode.Append).insertInto(hiveTableName)
     }
